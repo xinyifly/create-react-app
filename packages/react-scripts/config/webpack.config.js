7ec4ebcb99c2c606f6494fdf994adea0d9c0c868
Zeyu Chen <xinyifly@live.cn>
2020-05-09 16:47:51 +0800

    feat(react-scripts): config postcss-px-to-viewport

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -110,6 +110,12 @@ module.exports = function(webpackEnv) {
               },
               stage: 3,
             }),
+            require('postcss-px-to-viewport')({
+              viewportWidth: 1945,
+              mediaQuery: true,
+              sm: 959.98,
+              smWidth: 375,
+            }),
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
